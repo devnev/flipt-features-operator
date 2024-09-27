54d200100726e6323c5753afd017467e38fbe44d
Mark Nevill <mark.nevill@gmail.com>
2024-09-27 16:07:21 +0800

    Import flipt state types

--- api/v1alpha1/features_types.go
+++ api/v1alpha1/features_types.go
@@ -21,22 +21,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // FeaturesSpec defines the desired state of Features
 type FeaturesSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Features. Edit features_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Features Document `json:"features,omitempty"`
 }
 
 // FeaturesStatus defines the observed state of Features
 type FeaturesStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
